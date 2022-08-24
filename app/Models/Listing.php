ae9ee713e7aeae7b86a395a3d1f5a2f4ccaf8296
hvtk <hvtk1968@gmail.com>
2022-08-24 10:58:37 +0200

    added code till validation and store listing 2:27:20

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,6 +9,9 @@ class Listing extends Model
 {
     use HasFactory;
 
+    //it is also possible with AppServiceProvider in the function boot put Model::unguard()
+    protected $fillable= ['title', 'company', 'location', 'email', 'website', 'tags', 'description'];
+
     public function scopeFilter($query, array $filters) {
         if($filters['tag'] ?? false) {
             $query->where('tags', 'like', '%' . request('tag') . '%');
