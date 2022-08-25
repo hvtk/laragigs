864a923e967c8b96510e749b2c3f055d7847ae49
hvtk <hvtk1968@gmail.com>
2022-08-25 11:21:58 +0200

    added flash messages, course till 2:32:49

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,8 +29,8 @@
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
             <a href="/"
-                ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"
-            /></a>
+                ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"/>
+            </a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 <li>
                     <a href="register.html" class="hover:text-laravel"
@@ -61,5 +61,7 @@
 
         </a>
         </footer>
+
+        <x-flash-message />
     </body>
 </html>
