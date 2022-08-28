dbf2722b149501c06b0ff539cae5cfb5bff393eb
hvtk <hvtk1968@gmail.com>
2022-08-28 15:08:57 +0200

    added code for auth & guest middleware, course till 3:51:30

--- routes/web.php
+++ routes/web.php
@@ -101,34 +101,34 @@ Route::get('/search', function (Request $request) {
 Route::get('/', [ListingController::class, 'index']);
 
 // Show Create Form
-Route::get('/listings/create', [ListingController::class, 'create']);
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
 
 // Store Listing Data
-Route::post('/listings', [ListingController::class, 'store']);
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
 
 // Show Edit Form
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
 
 // Update Listing
-Route::put('/listings/{listing}', [ListingController::class, 'update']);
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
 
 // Destroy (delete) Listing
-Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
 
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
-// Show Register/Create Form
-Route::get('/register', [UserController::class, 'create']);
+// Show Register/Create Form (you have to change RouteServiceProvider.php '/home' into '/' because that is the used path in this project)
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
 // Create New User
 Route::post('/users', [UserController::class, 'store']);
 
 // Log User Out
-Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 
-// Show Login Form
-Route::get('/login', [UserController::class, 'login']);
+// Show Login Form (name 'login' links to Middleware authenticate)
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
 
 // Log In User
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
